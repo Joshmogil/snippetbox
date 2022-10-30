cbabd3f1e7f5feb05b72c5bcc6b2ae968730d099
Josh Mogil <jmogi@LAPTOP-4MBDPAMO.localdomain>
2022-10-30 19:14:31 -0400

    c createSnippet

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -3,7 +3,7 @@ package mysql
 import (
 	"database/sql"
 
-	"github.com/Joshmogil/snippetbox/pkg/model"
+	"github.com/Joshmogil/snippetbox/pkg/models"
 )
 
 // Define a model type which wraps a sql.DB connection pool
@@ -13,34 +13,23 @@ type SnippetModel struct {
 
 
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	// Write the SQL statement we want to execute. I've split it over two lines
-	// for readability (which is why it's surrounded with backquotes instead
-	// of normal double quotes).
+
 	stmt := `INSERT INTO snippets (title, content, created, expires)
 	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
-	// Use the Exec() method on the embedded connection pool to execute the
-	// statement. The first parameter is the SQL statement, followed by the
-	// title, content and expiry values for the placeholder parameters. This
-	// method returns a sql.Result object, which contains some basic
-	// information about what happened when the statement was executed.
+
 	result, err := m.DB.Exec(stmt, title, content, expires)
 	if err != nil {
 	return 0, err
 	}
-
-	// Use the LastInsertId() method on the result object to get the ID of our
-	// newly inserted record in the snippets table.
 	id, err := result.LastInsertId()
 	if err != nil {
-	return 0, err
+		return 0, err
 	}
-	// The ID returned has the type int64, so we convert it to an int type
-	// before returning.
 	return int(id), nil
-	
+}
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return 0, nil
+	return nil, nil
 }
 
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
