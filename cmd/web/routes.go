57b4c512ac313d91a4a33c547da905fdab46b65e
Josh Mogil <jmogil@pop-os.localdomain>
2022-12-27 17:35:25 -0500

    restful routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"github.com/justinas/alice"
+	"github.com/bmizerany/pat"
 )
 
 
@@ -10,11 +11,20 @@ func (app *application) routes() http.Handler {
 
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest,secureHeaders)
 
-	mux := http.NewServeMux()
+	/*mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
 	mux.HandleFunc("/snippet/create", app.createSnippet)
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	*/
+	mux := pat.New()
+	mux.Get("/",http.HandlerFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", http.HandlerFunc(app.createSnippet))
+	mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet)) // Moved down
+	
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
+
 	return standardMiddleware.Then(mux)
 }
