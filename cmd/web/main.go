94fa559a7caef722a54eabb4562274feb96be193
Josh Mogil <jmogil@pop-os.localdomain>
2023-01-04 21:21:57 -0500

    database models

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -27,6 +27,7 @@ type application struct {
 	session 	*sessions.Session
 	snippets 	*mysql.SnippetModel
 	templateCache map[string]*template.Template
+	users		*mysql.UserModel
 }
 
 func main() {
@@ -71,6 +72,7 @@ func main() {
 		session: 		session,
 		snippets:		&mysql.SnippetModel{DB: db},
 		templateCache: 	templateCache,
+		users: 			&mysql.UserModel{DB: db},
 	}
 
 	srv := &http.Server {
--- sql/snippets_table.sql
+++ sql/snippets_table.sql
@@ -12,6 +12,16 @@ expires DATETIME NOT NULL
 -- Add an index on the created column.
 CREATE INDEX idx_snippets_created ON snippets(created);
 
+USE snippetbox;
+CREATE TABLE users (
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+hashed_password CHAR(60) NOT NULL,
+created DATETIME NOT NULL
+);
+ALTER TABLE users ADD CONSTRAINT users_uc_email UNIQUE (email);
+
 INSERT INTO snippets (title, content, created, expires) VALUES (
 'An old silent pond',
 'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.',
